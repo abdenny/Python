4824c47af337b4bc1883ff1f240d74274d3889a1
Austin Denny <austinbdenny@gmail.com>
2020-01-16 11:53:42 -0600

    finished smal and medium problems

---
+++ exercise3.py
@@ -0,0 +1,90 @@
+##### 1/16
+### Small
+#1.
+# def madlib (name, subject):
+#     print(f"{name}\'s favorite subject is {subject}")
+
+# madlib("austin", "biology")
+
+
+#2.
+# def c_to_f (temp):
+#     F = (temp * 9/5) + 32
+#     print(F)
+
+# c_to_f(56)
+
+
+#3.
+# def f_to_c (temp):
+#     C = (temp - 32) * (5 / 9)
+#     print(C)
+
+# f_to_c(100)
+
+
+# 4. 
+# def is_even (number):
+#     if (number % 2) == 0:
+#         print(True)
+#     else:
+#         print(False)
+
+# is_even(35)
+
+
+# #5.
+# def is_odd (is_even):
+#     if is_even != True:
+#         print("The number is odd")
+
+# is_odd(is_even)
+
+
+#6.
+# def only_evens (list_of_numbers):
+#     even_numbers = []
+#     for i in list_of_numbers:
+#         if (i % 2) == 0:
+#             even_numbers.append(i)
+#     print(even_numbers)
+
+# only_evens([1,2,4,5,56,89,102])
+
+#7.
+# def only_odds (list_of_nums):
+#     odd_numbers = []
+#     for i in list_of_nums:
+#         if (i % 2) != 0:
+#             odd_numbers.append(i)
+#     print(odd_numbers)
+
+# only_odds([1,2,56,78,99,456,477])
+
+### Medium
+#1.
+# def smallest (list_nums):
+#     print(min(list_nums))
+
+# smallest([34,67,89,101])
+
+# #2.
+# def largest (list_nums):
+#     print(max(list_nums))
+
+# largest([34,67,89,101])
+
+#3. ### Using pythons key parameter to specify sorting value. Setting this equal to the len function to fid the shortest value.
+# def shortest (list_of_strings):
+#     print(min(list_of_strings, key=len))
+
+# shortest(["dog", "bird", "pizza"])
+
+#4.
+# def longest (list_of_strings):
+#     print(max(list_of_strings, key=len))
+
+# longest(["dog", "bird", "pizza"])
+
+###Large
+
